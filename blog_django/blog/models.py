158997f25dd3a22c4b156eadf41e1376c0577e34
BuildTools <unconfigured@null.spigotmc.org>
2023-10-16 21:44:36 +0000

    Working forms for the blog project

--- blog_django/blog/models.py
+++ blog_django/blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    
     author = models.ForeignKey(
         'auth.User',
-
         on_delete=models.CASCADE,
     )
 
@@ -12,3 +14,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
